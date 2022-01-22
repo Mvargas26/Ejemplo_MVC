ce4c142fd67bcaacd39bf078c893cc520bdb80ee
Michael Vargas Soto <michaelvargas47@hotmail.com>
2022-01-22 12:39:52 -0600

    Add project files.

---
+++ Ejemplo_MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Mision()
+        {
+            return View();
+        }
+    }
+}
+
+// cada metodo esta relacionado con un controlador, cda metodo contiene una vista y cada vista es un documento html
+// que esta en Home
---
+++ Ejemplo_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejemplo_MVC.MvcApplication" Language="C#" %>
